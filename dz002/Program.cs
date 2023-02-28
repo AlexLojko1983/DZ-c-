70a082e6d5aef63c0b95e1f8571d3d9f463ad780
AlexLojko <a_l_g@tut.by>
2023-02-28 22:43:08 +0300

    add task 2

--- dz002/Program.cs
+++ dz002/Program.cs
@@ -16,4 +16,36 @@ while (num <= 99 || num >= 1000);
 
 Console.WriteLine($"From the number {num}, the second digit {SecondNumber(num)}");
 */
-// Task2
+// Task2: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+void ThreeNumber (int number)
+{
+    if(number > 99)
+    {
+        int number_digit = 1;
+        int count = 0;
+        while (number/number_digit >= 1)
+        {
+            number_digit *= 10;
+            count++;                // Ищем разрядность числа
+        }
+        if(count > 3)
+        {            
+            int digit = number/Convert.ToInt32(Math.Pow(10,count-3))%10;
+            Console.WriteLine(digit);
+        }   
+        if(count == 3)
+        {
+            int digit = number%10;
+            Console.WriteLine(digit);
+        }        
+    }
+    else
+        Console.WriteLine("three digit not");
+}
+
+Console.Write("Input Number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+ThreeNumber(number);
+
+
