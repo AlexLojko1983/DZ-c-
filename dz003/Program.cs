46f178c41b8406934e48b11e8cc099061a99c2f8
AlexLojko <a_l_g@tut.by>
2023-03-03 23:19:12 +0300

    add Task2

--- dz003/Program.cs
+++ dz003/Program.cs
@@ -23,3 +23,29 @@ int Number(string message)
 int number = Number("Input five-digital number: ");
 Palindrom(number);
 */
+
+/* Task2: Напишите программу, которая принимает на вход координаты двух точек и 
+находит расстояние между ними в 3D пространстве.*/
+/*
+double Distance(double x1, double y1, double z1, double x2, double y2, double z2)   
+{
+    double distance = Math.Sqrt(Math.Pow(x2-x1, 2) + Math.Pow(y2-y1, 2) + Math.Pow(z2-z1, 2));
+    return distance;
+}
+double Input(string message)
+{   
+    Console.Write(message);
+    double a = Convert.ToDouble(Console.ReadLine());
+    
+    return a; 
+}
+
+double x1 = Input("Input coordinate x1: ");
+double y1 = Input("Input coordinate y1: ");
+double z1 = Input("Input coordinate z1: ");
+double x2 = Input("Input coordinate x2: ");
+double y2 = Input("Input coordinate y2: ");
+double z2 = Input("Input coordinate z2: ");
+
+Console.WriteLine(Math.Round(Distance(x1,y1,z1,x2,y2,z2),2));
+*/
